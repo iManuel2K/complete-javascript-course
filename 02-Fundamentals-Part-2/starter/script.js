425f616ba53cd0398812da5bc57bb6a1b84e4d29
Imanuel Harizi <iManuel2K@protonmail.com>
2022-07-14 11:51:20 +0200

    Challenge 1,2 & Arrays,Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-"use strict;";
+"use strict";
 
 // // Function Declaration
 // function calculateAge(birthYear) {
@@ -22,10 +22,69 @@
 
 // Exercise: Years until retirement
 // Big difference from "traditional" functions and arrow functions, is that arrow functions cant be found/used with "this"
-const birthYear = parseInt(prompt("What year were you born?"));
-const currentYear = new Date().getFullYear();
+// const birthYear = parseInt(prompt("What year were you born?"));
+// const currentYear = new Date().getFullYear();
 
-let retirementYearsLeft = (retirementAge, birthYear) =>
-    retirementAge - (currentYear - birthYear);
+// let retirementYearsLeft = (retirementAge, birthYear) =>
+//     retirementAge - (currentYear - birthYear);
 
-console.log(retirementYearsLeft(65, birthYear));
+// console.log(retirementYearsLeft(65, birthYear));
+
+function setup() {
+    const pcIsTurnedOn = turnPC(false);
+    if (pcIsTurnedOn) {
+        turnMonitor1();
+        turnMonitor2();
+        turnSpeakers();
+
+        return console.log("Setup is online, have fun!");
+    }
+
+    return console.log("Oops, your setup is still offline!");
+}
+
+function turnPC(boolean) {
+    return boolean;
+}
+
+function turnMonitor1() {
+    return console.log("Are you reaaaaady? Let's see some pixels!");
+}
+
+function turnMonitor2() {
+    return console.log("Monitor 2 is ready to be used to its maximum!");
+}
+
+function turnSpeakers() {
+    return console.log("Let's rock and roll!!");
+}
+
+setup();
+
+// Arrays
+const teams = ["Barca", "Real", "Man City", "Aris"];
+console.log(teams);
+
+teams.push("PSG");
+console.log(teams);
+
+teams.splice(2, 1);
+console.log(teams);
+
+console.log(teams.pop());
+
+teams.unshift("Besiktas");
+console.log(teams);
+
+teams.pop();
+console.log(teams);
+
+teams.shift();
+console.log(teams);
+
+console.log(teams.indexOf("Real"));
+console.log(teams.indexOf("Aris"));
+console.log(teams.includes("Real")); // === important for if statements
+console.log(teams.includes("Aris"));
+
+// Objects
