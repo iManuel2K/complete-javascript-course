3a420d3287564010e8ed366ad5911ab9e27bd9fd
Imanuel Harizi <iManuel2K@protonmail.com>
2022-07-15 12:45:47 +0200

    old fundamentals

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -5,18 +5,52 @@
 
 
 // 5 falsy values = 0, "", undefined, null, NaN
-const money = 0;
-const type = "ss";
+// const money = 0;
+// const type = "ss";
 
-console.log(money, type);
-if (money) {
-    console.log("Its not falsy");
-} else if (!money) {
-    console.log("Its actually falsy");
+// console.log(money, type);
+// if (money) {
+//     console.log("Its not falsy");
+// } else if (!money) {
+//     console.log("Its actually falsy");
+// }
+
+// if (type) {
+//     console.log("Its not falsy");
+// } else if (!type) {
+//     console.log("Its actually falsy");
+// }
+
+// switch
+// let userPrefferedDay = prompt("What day of the week you like the most?").toLowerCase();
+
+// switch (userPrefferedDay) {
+//     case "monday": // userPrefferedDay === "monday"
+//         console.log("Whaaaaaat?");
+//         break;
+//     case "tuesday":
+//         console.log("Never!");
+//         break;
+//     case "friday":
+//         console.log("Yes for all of us!");
+//         break;
+//     default:
+//         console.log("That is not a valid day!");
+//         break;
+// }
+
+// Expressions
+3 + 4
+1022123
+true && !false
+
+// Statements
+if (true) {
+    const CONSTANT123 = "Statement is this string"; // The whole "sentence" is a statement, because it doesnt produce a value, ends with semicolon ;
+    const literalsAreExpressions = `Because we use ${CONSTANT123}`;
 }
 
-if (type) {
-    console.log("Its not falsy");
-} else if (!type) {
-    console.log("Its actually falsy");
-}
+// Conditional Ternary Operator
+const age = 20;
+const ageType = age > 50 ? "Old man" : "Young man";
+console.log(`He is an ${ageType}`);
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// With enabled strict mode, console shows the error
+
+// let hasMoney = false;
+// let hasJob = true;
+
+// if (hasJob) hasmoney = true; 
+// if (hasMoney) console.log("Yes has money");
+
+// const interface = "Audio";
+
+// Functions
