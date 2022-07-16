941893c60994fa6cad1664ff94aca0da2117e2f9
Imanuel Harizi <iManuel2K@protonmail.com>
2022-07-16 14:35:03 +0200

    Project #2 finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let hiddenModal = document.getElementById('modal');
+let overlay = document.getElementById('overlay');
+let closeBtn = document.getElementById('closeButton');
+
+function hideModal() {
+  hiddenModal.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+let modals = document.querySelectorAll('.show-modal').forEach(modal => {
+  modal.addEventListener('click', function () {
+    hiddenModal.style.display = 'block';
+    overlay.style.display = 'block';
+  });
+});
+
+closeBtn.addEventListener('click', hideModal);
+
+hiddenModal.addEventListener('mouseleave', hideModal);
+
+addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    hideModal();
+  }
+});
