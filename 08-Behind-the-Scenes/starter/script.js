86cbf8af05fb553bc95e0d51324b0f330dc2fc4f
Imanuel Harizi <iManuel2K@protonmail.com>
2022-07-19 16:55:14 +0200

    Shallow, Deep cloning

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -46,19 +46,83 @@
 // };
 
 // const functionArr = (a, b) => a - b;
-let test = 0;
+// let test = 0;
 
-if (true) {
-  console.log('Calling from an if statement', this.test);
+// if (true) {
+//   console.log('Calling from an if statement', this.test);
 
-  if (true) {
-    console.log('Calling from a nested if statement', this.test);
-  }
-}
+//   if (true) {
+//     console.log('Calling from a nested if statement', this.test);
+//   }
+// }
 
-function getTest() {
-  console.log('Calling from a function', this.test);
-}
+// function getTest() {
+//   console.log('Calling from a function', this.test);
+// }
 
-this.getTest();
-getTest();
+// this.getTest();
+// getTest();
+
+// Primitive types, when changed they create a new a adress in the call stack
+// e.g. num, adress = 0001, value = 0
+// num2 = num => num2, adress = 0001, value 0
+// num2 = 100, num, adress = 0002, value 0 && num2, adress = 0001, value = 100
+let num = 0;
+let num2 = num;
+num = 100;
+console.log(num2);
+
+// Reference types, when changed they change the value for both objects because no new adress is created
+// These types use the Memory Heap
+// numObj, adress = 0003, value = D30F => Memory Heap => adress = D30F, value = {num = 50}
+// numObj2, adress = 0003, value = D30F => Memory Heap => adress = D30F, value = {num = 1000}
+// Now every object that is referenced to numObj has that num value
+const numObj = {
+  value: 50,
+};
+
+const numObj2 = numObj;
+numObj2.value = 1000;
+
+console.log(numObj.value, numObj2.value);
+
+let nameObj = {
+  name: 'Imanuel',
+};
+
+console.log(nameObj);
+
+const nameObj2 = nameObj;
+nameObj2.name = 'Imanuel 2.0';
+
+console.log(nameObj, nameObj2);
+nameObj.name = 'Enkelejd';
+console.log(nameObj, nameObj2);
+
+nameObj = {};
+console.log(nameObj);
+
+//Shallow copy/clone, but is the object has another object it wont work
+const shallow = {
+  name: 'Something',
+  shallow3: {
+    name: 'Shallow 3',
+  },
+};
+
+const shallow2 = Object.assign({}, shallow);
+console.log(shallow, shallow2);
+
+shallow2.name = 'Else';
+console.log(shallow, shallow2);
+
+// You need to deep clone for this to work
+shallow2.shallow3.name = 'HOHOHO';
+console.log(shallow, shallow2);
+
+// Deepl clone attempt
+const shallowDeep = structuredClone(shallow);
+console.log(shallowDeep);
+
+shallowDeep.shallow3.name = 'Ufffff';
+console.log(shallow, shallow2, shallowDeep);
