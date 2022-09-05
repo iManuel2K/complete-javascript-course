14d811b30423815562496bf2d0692f9ac7bcfd2c
Imanuel Harizi <iManuel2K@protonmail.com>
2022-09-05 20:56:04 +0200

    High order functions, abstraction

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,3 +25,26 @@ const checkIn = function (flightNum, passenger) {
 };
 
 checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+// Abstraction
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
